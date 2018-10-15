b6e0893a90da253e3554dc3f8e92d6728a8a890d
Kevin Zheng <kevin.zheng4783@gmail.com>
2018-10-15 19:17:55 -0400

    completed quiz

---
+++ quiz1.js
@@ -0,0 +1,13 @@
+/*
+  @func avgTwoNumbers
+  @param a {number}
+  @param b {number}
+  @returns {number}
+  @desc - computes average of two numbers
+          formular for average: a + b / 2
+  @example avgTwoNumbers(1,2); // 1.5
+*/
+const avgTwoNumbers = (num1,num2) => (num1 + num2)/2;
+
+console.log(avgTwoNumbers(1,2));
+console.log(avgTwoNumbers(5,10));

b3f3feb67b2aafbd15bf6fc6ba0ff343a6832bee
Van Tran <vantran@pursuit.org>
2018-10-15 19:39:19 -0400

    VanTran

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+  @func avgTwoNumbers
+  @param a {number}
+  @param b {number}
+  @returns {number}
+  @desc - computes average of two numbers
+          formular for average: a + b / 2
+  @example avgTwoNumbers(1,2); // 1.5
+*/
+
+function avgTwoNumbers(a,b) {
+    if (a + b / 2)
+return (a,b);
+}
+
+console.log(avgTwoNumbers(1,2));

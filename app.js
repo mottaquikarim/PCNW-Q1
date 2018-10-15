8dae46c2ce539c7ccc6c806b21e9cb981e814d1c
ositaigwe@pursuit.org <j0h@example.com>
2018-10-15 19:23:58 -0400

    additional example

--- app.js
+++ app.js
@@ -10,9 +10,9 @@
 
 const avgTwoNumbers = (a, b) => {
    if (typeof a == "number" && typeof b == "number") {
-       return ((a + b )/2);
+       return ((a + b)/2);
    }
-        return "please input two numbers"
+        return "please input two numbers";
 }
 console.log(avgTwoNumbers(1, 2));
 console.log(avgTwoNumbers(10, 12));
@@ -20,6 +20,8 @@ console.log( avgTwoNumbers("apples", "oranges"));
 console.log( avgTwoNumbers(1000, "oranges"));
 console.log( avgTwoNumbers(1000, true));
 console.log( avgTwoNumbers(500, -200))
+console.log(avgTwoNumbers(3, 5));
+
 
 
 

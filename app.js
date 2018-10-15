63879850ef11b0d3291a2eaea3ebe64e419232ed
Karan Taneja <karantaneja@pursuit.org>
2018-10-15 19:22:55 -0400

    Finished the quiz

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+  @func avgTwoNumbers
+  @param a {number}
+  @param b {number}
+  @returns {number}
+  @desc - computes average of two numbers
+          formular for average: a + b / 2
+  @example avgTwoNumbers(1,2); // 1.5
+*/
+
+
+const avgTwoNumbers = (num1, num2) => {
+
+    return (num1 + num2) / 2
+
+}
+
+console.log(avgTwoNumbers(1, 2))
